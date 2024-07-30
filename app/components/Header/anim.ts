b9c98604ea2062d675d768d8ae60e9492487ec67
Znorlux <christianlombardi0414@gmail.com>
2024-07-29 22:08:01 -0500

    Eliminado elementos innecesarios para  crear header de stairs

--- app/components/Header/anim.ts
+++ app/components/Header/anim.ts
@@ -75,3 +75,39 @@ export const translate = {
     transition: { duration: 0.7, ease: [0.76, 0, 0.24, 1], delay: i[1] },
   }),
 };
+
+export const slideLeft = {
+  initial: {
+    x: 150,
+  },
+  enter: {
+    x: 0,
+    transition: { duration: 0.5, ease: [0.33, 1, 0.68, 1] },
+  },
+  exit: {
+    x: 150,
+    transition: { duration: 0.5, ease: [0.33, 1, 0.68, 1] },
+  },
+};
+
+export const rotateX = {
+  initial: {
+    rotateX: 90,
+    opacity: 0,
+  },
+  enter: (i: number) => ({
+    rotateX: 0,
+    opacity: 1,
+    transition: {
+      duration: 0.5,
+      ease: [0.33, 1, 0.68, 1],
+      delay: 0.3 + i * 0.05,
+    },
+  }),
+  exit: {
+    opacity: 0,
+    transition: { duration: 0.5, ease: [0.33, 1, 0.68, 1] },
+  },
+};
+
+export const mountAnim = { initial: "initial", animate: "enter", exit: "exit" };
