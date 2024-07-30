4f1c8fde2d87a419dc39549f4305b27df24536ea
Znorlux <christianlombardi0414@gmail.com>
2024-07-29 21:19:08 -0500

    chore: Update header component with client-side rendering

--- app/components/Header/Header.tsx
+++ app/components/Header/Header.tsx
@@ -1,7 +1,59 @@
-import React from "react";
+"use client";
+import styles from "./Header.module.scss";
+import Link from "next/link";
+import { useState } from "react";
+import { motion, AnimatePresence } from "framer-motion";
+import { opacity, background } from "./anim";
+//import Nav from "./nav";
 
-function Header() {
-  return <div>Header</div>;
+export default function Header() {
+  const [isActive, setIsActive] = useState(false);
+
+  return (
+    <div className={styles.header}>
+      <div className={styles.bar}>
+        <Link href="/">Logo</Link>
+        <div
+          onClick={() => {
+            setIsActive(!isActive);
+          }}
+          className={styles.el}
+        >
+          <div
+            className={`${styles.burger} ${
+              isActive ? styles.burgerActive : ""
+            }`}
+          ></div>
+          <div className={styles.label}>
+            <motion.p
+              variants={opacity}
+              animate={!isActive ? "open" : "closed"}
+            >
+              Menu
+            </motion.p>
+            <motion.p variants={opacity} animate={isActive ? "open" : "closed"}>
+              Close
+            </motion.p>
+          </div>
+        </div>
+        <motion.div
+          variants={opacity}
+          animate={!isActive ? "open" : "closed"}
+          className={styles.shopContainer}
+        >
+          <p className={styles.shop}>Shop</p>
+          <div className={styles.el}>
+            <p>Portfolio</p>
+          </div>
+        </motion.div>
+      </div>
+      <motion.div
+        variants={background}
+        initial="initial"
+        animate={isActive ? "open" : "closed"}
+        className={styles.background}
+      ></motion.div>
+      {/*<AnimatePresence mode="wait">{isActive && <Nav />}</AnimatePresence>*/}
+    </div>
+  );
 }
-
-export default Header;
