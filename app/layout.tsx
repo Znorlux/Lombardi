27cff74ee029d13b89a7369966c77b7c657ad424
Znorlux <christianlombardi0414@gmail.com>
2024-07-31 16:43:06 -0500

    feat: Implement Loader component for the home page

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "./components/Header/Header";
-import StairsHeader from "./components/StairsHeader";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,7 +19,6 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <Header />
-        {/*<StairsHeader />*/}
         {children}
       </body>
     </html>
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,53 @@
-import Image from "next/image";
+"use client";
+import { useLayoutEffect, useState } from "react";
+import { gsap } from "gsap";
+import Hero from "./components/Home/Hero";
+import Loader from "./components/Loader";
+
+type Timeline = gsap.core.Timeline;
 
 export default function Home() {
+  const [loaderFinished, setLoaderFinished] = useState(false);
+  const [timeline, setTimeline] = useState<Timeline | null>(null);
+  const [checkingSession, setCheckingSession] = useState(true);
+
+  useLayoutEffect(() => {
+    // Comprobar si ya se ha cargado la página antes
+    const hasLoaded = sessionStorage.getItem("hasLoaded");
+
+    if (hasLoaded) {
+      // Si ya se ha cargado antes, omitir la animación del Loader
+      setLoaderFinished(true);
+      setCheckingSession(false);
+    } else {
+      // Si no se ha cargado antes, ejecutar la animación del Loader
+      const context = gsap.context(() => {
+        const tl = gsap.timeline({
+          onComplete: () => {
+            setLoaderFinished(true);
+            // Guardar el estado en sessionStorage
+            sessionStorage.setItem("hasLoaded", "true");
+            setCheckingSession(false);
+          },
+        });
+        setTimeline(tl as Timeline);
+      });
+
+      return () => context.revert();
+    }
+  }, []);
+
+  {
+    /*if (checkingSession) {
+    return (
+      <div className="flex items-center justify-center h-screen">
+        Cargando...
+      </div>
+    );
+  }*/
+  }
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Lombardi
-    </main>
+    <main>{loaderFinished ? <Hero /> : <Loader timeline={timeline} />}</main>
   );
 }
