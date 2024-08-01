d016cd4577e5585d06efdaa7bd443b6529ad76f3
Znorlux <christianlombardi0414@gmail.com>
2024-07-31 21:53:17 -0500

    Implementacion correcta de Loader 1 sola vez con sessionStorage

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useLayoutEffect, useState } from "react";
+import { useLayoutEffect, useState, useEffect } from "react";
 import { gsap } from "gsap";
 import Hero from "./components/Home/Hero";
 import Loader from "./components/Loader";
@@ -7,45 +7,30 @@ import Loader from "./components/Loader";
 type Timeline = gsap.core.Timeline;
 
 export default function Home() {
-  const [loaderFinished, setLoaderFinished] = useState(false);
+  const [loaderFinished, setLoaderFinished] = useState(true); // Inicialmente true para evitar parpadeo
   const [timeline, setTimeline] = useState<Timeline | null>(null);
-  const [checkingSession, setCheckingSession] = useState(true);
+
+  useEffect(() => {
+    const hasLoaderFinished =
+      sessionStorage.getItem("loaderFinished") === "true";
+    setLoaderFinished(hasLoaderFinished);
+  }, []);
 
   useLayoutEffect(() => {
-    // Comprobar si ya se ha cargado la página antes
-    const hasLoaded = sessionStorage.getItem("hasLoaded");
-
-    if (hasLoaded) {
-      // Si ya se ha cargado antes, omitir la animación del Loader
-      setLoaderFinished(true);
-      setCheckingSession(false);
-    } else {
-      // Si no se ha cargado antes, ejecutar la animación del Loader
+    if (!loaderFinished) {
       const context = gsap.context(() => {
         const tl = gsap.timeline({
           onComplete: () => {
             setLoaderFinished(true);
-            // Guardar el estado en sessionStorage
-            sessionStorage.setItem("hasLoaded", "true");
-            setCheckingSession(false);
+            sessionStorage.setItem("loaderFinished", "true");
           },
         });
-        setTimeline(tl as Timeline);
+        setTimeline(tl);
       });
 
       return () => context.revert();
     }
-  }, []);
-
-  {
-    /*if (checkingSession) {
-    return (
-      <div className="flex items-center justify-center h-screen">
-        Cargando...
-      </div>
-    );
-  }*/
-  }
+  }, [loaderFinished]);
 
   return (
     <main>{loaderFinished ? <Hero /> : <Loader timeline={timeline} />}</main>
