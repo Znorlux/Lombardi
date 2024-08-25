7a9774d5d24c3d6acaa28397e307a3a2c217f7c9
Znorlux <christianlombardi0414@gmail.com>
2024-08-24 22:23:52 -0500

    feat: Optimize mouse enter and leave event handling in LinkComponent

--- app/components/Header/nav/Menu/Link.tsx
+++ app/components/Header/nav/Menu/Link.tsx
@@ -19,21 +19,10 @@ export default function LinkComponent({ data, index }: LinkProps) {
   const { title, description, images } = data;
   const outer = useRef<HTMLDivElement>(null);
   const inner = useRef<HTMLDivElement>(null);
-  const [isReady, setIsReady] = useState(false);
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setIsReady(true);
-    }, 1000);
-
-    return () => clearTimeout(timer);
-  }, []);
 
   const manageMouseEnter = (
     e: React.MouseEvent<HTMLDivElement, MouseEvent>
   ) => {
-    if (!isReady) return;
-
     const bounds = e.currentTarget.getBoundingClientRect();
     if (e.clientY < bounds.top + bounds.height / 2) {
       gsap.set(outer.current, { top: "-100%" });
@@ -49,8 +38,6 @@ export default function LinkComponent({ data, index }: LinkProps) {
   const manageMouseLeave = (
     e: React.MouseEvent<HTMLDivElement, MouseEvent>
   ) => {
-    if (!isReady) return;
-
     const bounds = e.currentTarget.getBoundingClientRect();
     if (e.clientY < bounds.top + bounds.height / 2) {
       gsap.to(outer.current, { top: "-100%", duration: 0.3 });
